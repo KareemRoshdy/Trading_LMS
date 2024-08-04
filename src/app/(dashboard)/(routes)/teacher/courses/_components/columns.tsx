b066853453f92339909f60891be1d9da753f7ac5
KareemRoshdy <karem22roshdy@gmail.com>
2024-08-04 18:17:14 +0300

    update format in data table

--- src/app/(dashboard)/(routes)/teacher/courses/_components/columns.tsx
+++ src/app/(dashboard)/(routes)/teacher/courses/_components/columns.tsx
@@ -46,9 +46,9 @@ export const columns: ColumnDef<Course>[] = [
     cell: ({ row }) => {
       const price = parseFloat(row.getValue("price")) || "0";
 
-      const formatted = new Intl.NumberFormat("en-US", {
+      const formatted = new Intl.NumberFormat("en-EG", {
         style: "currency",
-        currency: "USD",
+        currency: "EGP",
       }).format(price);
       return <div>{formatted}</div>;
     },
